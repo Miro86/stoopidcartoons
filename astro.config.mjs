636fc61b38638fbfce019a45208438c1c913c7ae
miro <miroslav.zoranovic@codebluestudio.com>
2026-03-21 01:27:52 +0100

    Switch Astro site config to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://miro86.github.io',
-  base: '/stoopidcartoons',
+  site: 'https://www.stoopidcartoons.com',
+  base: '/',
   trailingSlash: 'always',
   integrations: [sitemap()],
   vite: {
